840cc888d41124f26c971c67e2580e4f029de935
Natsukawa Taichi <qmskbaevyhnkpbqibveycbrmmxtsjomc@gmail.com>
2022-04-07 14:18:42 +0900

    remove bitnation

--- bn/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <bn/main.hpp>
-
-int main(int argc, char *argv[]) {
-    bn::App app(argc, argv);
-    return app.run();
-}
--- bn/main.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <bn/header.hpp>
-#include <bn/app.hpp>
